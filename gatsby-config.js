77d65b4dfacca3e6e1f99f0761c9334e64bc69cb
mohamedim <52673620+mohamedim@users.noreply.github.com>
2019-07-09 09:22:49 +0200

    Add files via upload

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,8 +12,8 @@ module.exports = {
         name: 'gatsby-starter-default',
         short_name: 'starter',
         start_url: '/',
-        background_color: 'red',
-        theme_color: 'yellow',
+        background_color: '#663399',
+        theme_color: '#663399',
         display: 'minimal-ui',
         icon: 'src/assets/images/website-icon.png', // This path is relative to the root of the site.
       },
