887735968ae1f63eee1a44bb700068413d874b89
mohamedim <52673620+mohamedim@users.noreply.github.com>
2019-07-12 12:23:46 +0200

    Add files via upload

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 
-import logo from '../assets/images/logo.svg';
+import logo from '../assets/images/logo.jpg';
 
 const Header = (props) => (
     <header id="header" className="alt">
